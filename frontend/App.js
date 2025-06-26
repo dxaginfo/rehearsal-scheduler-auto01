4526cff5305adebd65cf9e6179f38722f91d0e45
dxaginfo <142343166+dxaginfo@users.noreply.github.com>
2025-06-26 07:21:24 -0300

    Add basic React front-end App component

---
+++ frontend/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div style={{padding:'2em', fontFamily:'sans-serif'}}>
+      <h1>Rehearsal Scheduler</h1>
+      <p>Simple responsive scheduling for bands.</p>
+      {/* Components for schedule creation, RSVP, etc. */}
+    </div>
+  )
+}
